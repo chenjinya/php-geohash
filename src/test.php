175d7de7c31a4dd96b4c55a7fb7d26916ea38db6
Jinya Chen <jinyachen@gmail.com>
2017-12-08 17:32:56 +0800

    update

--- src/test.php
+++ src/test.php
@@ -15,18 +15,27 @@ $lat = "40.0639144661348";
 $instance = new GeoHash();
 
 t("sample:");
+
 var_dump([
     "lng" => $lng,
     "lat" => $lat,
 ]);
+
 t("encode test:");
+
 $geohash = $instance->encode($lng, $lat);
 var_export($geohash);
+
+t("neighbors test:");
+
+$neighbors = $instance->neighbors($geohash);
+var_export($neighbors);
+
 t("bound test:");
+
 var_export($instance->neighbors('zzzzzz'));
 
 t("decode test:");
-
 
 $precision = 30;
 $binLng = $instance->binEncode($lng, $instance::LNG_MIN, $instance::LNG_MAX, $precision);
